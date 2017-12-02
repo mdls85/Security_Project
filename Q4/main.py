bc297abb786529763f2862d36b541117201ba3f2
Nicholas <snickdx@gmail.com>
2017-12-02 17:25:19 -0400

    updated read me and q4

--- Q4/main.py
+++ Q4/main.py
@@ -1,13 +1,33 @@
 # COMP 6901 Project Question 4
+import sys
 
 from MyDES import MyDES
 from MyElgamal import MyElgamal
-import json
+
+prime = 7127
+
+
+def write_cipter(c):
+    cfile = open("cipher.dat", "w")
+    for item in c:
+        cfile.write(str(item[0])+"\n")
+        cfile.write(str(item[1])+"\n")
+    cfile.close()
+
+
+def read_cipher():
+    cfile = open("cipher.dat", "r")
+    words = cfile.readlines()
+    c = []
+    for i in range(0, len(words)-1, 2):
+        c.append((int(words[i]), int(words[i+1])))
+    return c
 
 
 def write_private_key(key):
     key_file = open("privatekey.dat", 'w')
     key_file.write(key)
+    key_file.close()
 
 
 def read_private_key():
@@ -26,21 +46,19 @@ def read_public_key():
     return [int(arr[0]), int(arr[1]), int(arr[2])]
 
 
-def main():
-    key = b'8bytekey'
+def generate_keys(key):
+    global prime
+    if key is 'nil' or len(key) != 8:
+        raise ValueError("Incorrect password size!")
     iv = MyDES.get_iv()
     des_obj = MyDES(key, iv)
 
     print "Random IV: " + iv
-
-    prime = 7127
-
     el_obj = MyElgamal(prime)
 
     private_key = el_obj.gen_private_key(prime)
 
     print "Private Key: " + str(private_key)
-
     enc_private_key = des_obj.encrypt(plain_text=str(private_key))
 
     public_key = el_obj.get_public_key(private_key)
@@ -51,21 +69,30 @@ def main():
     print "Encrypted Private Key: " + enc_private_key
 
     write_private_key(enc_private_key)
-    print("Wrote Pub Key to file")
+    print("Wrote Pub Key to file privatekey.dat")
 
     write_public_key(public_key)
-    print("Wrote Encrypted Private Key to file")
+    print("Wrote Encrypted Private Key to file publickey.dat")
 
 
+def encrypt(plain_text):
     public_key = read_public_key()
     print "Read public key from file"
     print public_key
-
     el_obj2 = MyElgamal(public_key[1])
-    plain_text = "Hi Trump"
     c = el_obj2.encrypt(plain_text, public_key)
     print "Cipher"
     print c
+    write_cipter(c)
+    print "Cipher Written to file"
+
+
+def decrypt():
+    global prime
+    el_obj2 = MyElgamal(prime)
+    c = read_cipher()
+    print ("Cipher Read from file")
+    print(c)
 
     iv = raw_input("Enter IV: ")
     key = raw_input("Enter Key: ")
@@ -74,9 +101,31 @@ def main():
     enc_private_key = read_private_key()
     print "Read encrypted private key from file"
     private_key = int(des_obj2.decrypt(enc_private_key))
-    print "Decrypted private key from "+enc_private_key + " to " + str(private_key)
+    print "Decrypted private key from " + enc_private_key + " to " + str(private_key)
 
-    print "Decrypted Message: " + (el_obj.decrypt(c, private_key))
+    print "Decrypted Message: " + (el_obj2.decrypt(c, private_key))
+
+
+def main():
+
+    options = {
+        'generate-keys': lambda arg: generate_keys(arg),
+        'encrypt': lambda arg: encrypt(arg),
+        'decrypt': lambda arg: decrypt()
+    }
+
+    num_args = len(sys.argv)
+    try:
+        if num_args == 1:
+            print("No arguments supplied! run with flags generate-keys {password}, encrypt {message}, decrypt")
+        else:
+            if sys.argv[1] in options:
+                argm = "nil" if num_args < 3 else sys.argv[2]
+                options[sys.argv[1]](argm)  # fancy switch workaround
+            else:
+                print('invalid option')
+    except ValueError:
+        print("Bad password, IV or arguments supplied")
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-Python scripts to detect Java Deserialization vulnerability in Oracle Web Logic
+# COMP 6801 Project
 
-Requires python-libnmap and nmap
+##  Members
+ * Matthew Stewart
+ * Nicholas Mendez 811002795
 
-sudo apt-get install nmap
-pip install python-libnmap
+## Dependencies
+1. Linux Distro for Q1
+2. Python 2.7
+
+## Installation
+
+1. install nmap ``` $sudo apt-get install nmap```
+2. pip dependencies ``` $ sudo pip install -r requirements.txt```
+
+## Question 1
+
+Run Python scripts to detect Java Deserialization vulnerability in Oracle Web Logic
+
+
+## Question 4
+1. Generate keys using 8 character password also take note of the IV generated (IMPORTANT) 
+
+    ```$ python main.py generate-keys {8 character password}```
+2. Make sure publickey.dat is in the directory then encrypt a message (enclosed in quotes)
+ 
+    ```$ python main.py encrypt "{enter message here}" ```
+3. Make sure privatekey.dat and cipher.dat is in the directory and decrypt the message enter the previously mentioned generated IV and password when prompted.
+    
+    ```$ python main.py decrypt```
+    
+
+## Question 6
+
+1. Go To [Demo Link](https://snickdx.me/xss) on chrome.
+2. Open DevTools Javascript Console (F12).
+3. Login With username: "bob", password "bob".
+4. Create a comment "<script src="https://snickdx.me/xss/scammer.js"></script>" to perform xss.
+5. Console should show message "Thanks for the data... loser" showing xss was successful.
+6. Delete previous comment.
+7. Click the "XSS Protection Toggle" then recreate comment.
+8. The comment should be displayed and the console should indicate that it has been sanitized.
